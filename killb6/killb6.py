c6b2b7295beda797dd64206bb298c4f9077a65d0
Lasse Bromose <lbromo@protonmail.ch>
2021-03-29 20:33:46 +0200

    face detection seems to work

--- killb6/killb6.py
+++ killb6/killb6.py
@@ -10,10 +10,10 @@ import matplotlib.pyplot as plt
 min_size      = (50, 100) #(10, 10) # (50, 100)#
 image_scale   = 2
 haar_scale    = 1.1 #1.2 #1.1
-min_neighbors = 5 #2 #5
+min_neighbors = 2 #5
 haar_flags    = cv2.CASCADE_SCALE_IMAGE #0 #cv2.CASCADE_SCALE_IMAGE
 
-haarfile = '/usr/share/opencv4/haarcascades/haarcascade_upperbody.xml'
+haarfile = '/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
 
 cascade = cv2.CascadeClassifier()
 cascade.load(haarfile)
@@ -41,24 +41,17 @@ class MinimalPublisher(Node):
         # convert color input image to grayscale
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # scale input image for faster processing
-        small_img = cv2.resize(gray, new_size, interpolation = cv2.INTER_LINEAR)
-
-        small_img = cv2.equalizeHist(small_img)
-        
-        if(cascade):
-            faces = cascade.detectMultiScale(small_img, haar_scale, min_neighbors, haar_flags, min_size)
-            if faces is not None:
-                for (x, y, w, h) in faces:
-                    # the input to detectMultiScale was resized, so scale the
-                    # bounding box of each face and convert it to two CvPoints
-                    pt1 = (int(x * image_scale), int(y * image_scale))
-                    pt2 = (int((x + w) * image_scale), int((y + h) * image_scale))
-                    cv2.rectangle(img, pt1, pt2, (255, 0, 0), 3, 8, 0)
+        # Detect the faces
+        faces = cascade.detectMultiScale(gray, 1.1, 4)
+        # Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
         plt.figure()
         plt.imshow(img)
-        plt.show()
+        plt.pause(1)
+        plt.show(block=False)
+        plt.close()
         #cv2.imshow("result", img)
 
         msg = String()
